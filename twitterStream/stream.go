e86ab8435c6353fcd5f345b13c6c167522df8c43
ryusei1068 <nhrryi@gmail.com>
2022-04-17 23:17:41 +0900

    start streaming

--- twitterStream/stream.go
+++ twitterStream/stream.go
@@ -1,10 +1,13 @@
 package twitterstream
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 
 	twitterstream "github.com/fallenstedt/twitter-stream"
 	"github.com/fallenstedt/twitter-stream/rules"
+	"github.com/fallenstedt/twitter-stream/stream"
 )
 
 type (
@@ -14,8 +17,77 @@ type (
 	ItwitterStream interface {
 		AddRules(string) (*rules.TwitterRuleResponse, error)
 		GetRules() (*rules.TwitterRuleResponse, error)
+		InitiateStream()
 	}
 )
+
+type StreamData struct {
+	Data struct {
+		Text      string    `json:"text"`
+		ID        string    `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		AuthorID  string    `json:"author_id"`
+	} `json:"data"`
+	Includes struct {
+		Users []struct {
+			ID       string `json:"id"`
+			Name     string `json:"name"`
+			Username string `json:"username"`
+		} `json:"users"`
+	} `json:"includes"`
+	MatchingRules []struct {
+		ID  string `json:"id"`
+		Tag string `json:"tag"`
+	} `json:"matching_rules"`
+}
+
+func (t *TwitterStream) fetchTweets() stream.IStream {
+	api := t.api.Stream
+
+	var err error
+	api.SetUnmarshalHook(func(bytes []byte) (interface{}, error) {
+		data := StreamData{}
+
+		if err = json.Unmarshal(bytes, &data); err != nil {
+			fmt.Printf("failed to unmarshal bytes: %v", err)
+		}
+		return data, err
+	})
+
+	streamExpansions := twitterstream.NewStreamQueryParamsBuilder().
+		AddExpansion("author_id").
+		AddTweetField("created_at").
+		Build()
+
+	err = api.StartStream(streamExpansions)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return api
+}
+
+func (t *TwitterStream) InitiateStream() {
+	fmt.Println("Starting Stream")
+
+	api := t.fetchTweets()
+
+	defer t.InitiateStream()
+
+	for tweet := range api.GetMessages() {
+		if tweet.Err != nil {
+			fmt.Printf("got error from twitter: %v", tweet.Err)
+			api.StopStream()
+			continue
+		}
+
+		result := tweet.Data.(StreamData)
+		fmt.Println(result.Data)
+	}
+
+	fmt.Println("Stopped Stream")
+}
 
 func (t *TwitterStream) AddRules(key string) (*rules.TwitterRuleResponse, error) {
 	rules := twitterstream.NewRuleBuilder().AddRule(key, "-is:retweet").Build()
