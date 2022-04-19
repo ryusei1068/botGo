85332d8e06d679800ee97608fb5fa3ccbaad7527
ryusei1068 <nhrryi@gmail.com>
2022-04-20 08:10:32 +0900

    changed structure

--- twitterStream/directInfo.go
+++ twitterStream/directInfo.go
@@ -1,13 +1,18 @@
 package twitterstream
 
 type Association struct {
-	word     string
-	url      string
-	streamId string
+	word string
+	url  string
+}
+
+type Keys struct {
+	streamId  string
+	webhookId string
+	tag       string
 }
 
 // WebHookId, Key word, url
-var DirectInfo = make(map[string]Association)
+var DirectInfo = make(map[Keys]Association)
 
 const (
 	webhook = "https://discord.com/api/webhooks/"
