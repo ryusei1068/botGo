253c503a82ccfa015f85dea51fcc10cd042cb38b
ryusei1068 <nhrryi@gmail.com>
2022-04-19 19:50:34 +0900

    http response handle

--- httpClient/httpResponseParse.go
+++ httpClient/httpResponseParse.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type JsonData struct {
@@ -15,20 +17,6 @@ type JsonData struct {
 }
 
 func (c *httpClient) ParseJson(opts *RequestOpts, resp *http.Response) (JsonData, error) {
-	if resp.StatusCode >= 400 {
-		log.Printf("Network Request at %s failed: %v", opts.Url, resp.StatusCode)
-
-		var msg string
-		if resp.Body != nil {
-			body, _ := ioutil.ReadAll(resp.Body)
-			msg = "Network request failed: " + string(body)
-		} else {
-			msg = "Network request failed with status" + fmt.Sprint(resp.StatusCode)
-		}
-
-		return JsonData{}, errors.New(msg)
-	}
-
 	var webhooks []Webhook // GET
 	var webhook Webhook    // POST
 	data := JsonData{WebHooks: webhooks, WebHook: webhook}
@@ -52,4 +40,53 @@ func (c *httpClient) ParseJson(opts *RequestOpts, resp *http.Response) (JsonData
 	}
 
 	return data, nil
+}
+
+func (c *httpClient) HandleResponse(resp *http.Response, opts *RequestOpts, fn func(opts *RequestOpts) (*http.Response, error)) (*http.Response, error) {
+	if resp.StatusCode == 429 {
+		log.Printf("Retrying network request %s with backoff", opts.Url)
+
+		var msg string
+		if resp.Body != nil {
+			body, _ := ioutil.ReadAll(resp.Body)
+			msg = "Network request failed: " + string(body)
+		} else {
+			msg = "Network request failed with status: " + fmt.Sprint(resp.StatusCode)
+		}
+		log.Printf(msg)
+
+		var delay time.Duration = 30
+		var retrySec int
+		retry := resp.Header.Get("retry-after")
+		if len(retry) > 0 {
+			intVar, err := strconv.Atoi(retry)
+			if err != nil {
+				retrySec = 30
+			} else {
+				retrySec = intVar
+			}
+			delay = time.Duration(retrySec)
+		}
+
+		log.Printf("Sleeping for %v seconds", delay)
+		time.Sleep(delay)
+
+		return fn(opts)
+	}
+
+	if resp.StatusCode >= 400 {
+		log.Printf("Network Request at %s failed: %v", opts.Url, resp.StatusCode)
+
+		var msg string
+		if resp.Body != nil {
+			body, _ := ioutil.ReadAll(resp.Body)
+			msg = "Network request failed: " + string(body)
+		} else {
+			msg = "Network request failed with status" + fmt.Sprint(resp.StatusCode)
+		}
+
+		return nil, errors.New(msg)
+	}
+
+	return resp, nil
 }
