943d97ce45f40480d65d7ec3ed2996caf5bdb990
ryusei1068 <nhrryi@gmail.com>
2022-04-25 06:22:02 +0900

    registrate to redis server

--- redis/cluster.go
+++ redis/cluster.go
@@ -2,8 +2,9 @@ package redis
 
 import (
 	"context"
+	"encoding/json"
+	"log"
 
-	"github.com/botGo/config"
 	"github.com/go-redis/redis/v8"
 )
 
@@ -14,31 +15,75 @@ type (
 	}
 
 	IRedis interface {
-		SetValue(string, string) error
-		GetValues(string) []string
+		SetValues(string, []DirectInfo) error
+		GetValues(string) ([]DirectInfo, error)
 		GetKeys(string) []string
+		getRange(string) int
+	}
+
+	DirectInfo struct {
+		WebhookUrl       string `json:"webhookurl"`
+		WebhookId        string `json:"webhookId"`
+		WebhookToken     string `json:"webhooktoken"`
+		DiscordChannelId string `json:"discordChannleid"`
 	}
 )
 
-func (rc *RedisClient) SetValue(key string, value string) error {
-	err := rc.redisClient.Set(rc.ctx, key, value, 0).Err()
-	if err != nil {
-		return err
+func (rc *RedisClient) SetValues(key string, values []DirectInfo) error {
+	for i := range values {
+		json, err := json.Marshal(&values[i])
+		if err != nil {
+			log.Printf("failed to convert type ([]byte), %v", err)
+			return err
+		}
+
+		err = rc.redisClient.RPush(rc.ctx, key, json).Err()
+		if err != nil {
+			log.Printf("failed to add data, %v", err)
+			return err
+		}
 	}
+
 	return nil
 }
 
-func (rc *RedisClient) GetValues(key string) []string {
-	return []string{}
+func (rc *RedisClient) getRange(key string) int {
+	ldata := rc.redisClient.LLen(rc.ctx, key)
+	return int(ldata.Val())
+}
+
+func (rc *RedisClient) GetValues(key string) ([]DirectInfo, error) {
+	endIndex := rc.getRange(key)
+	val, err := rc.redisClient.LRange(rc.ctx, key, 0, int64(endIndex)).Result()
+
+	if err != nil {
+		log.Printf("failed to get values, %v", err)
+		return nil, err
+	}
+
+	var directInfo DirectInfo
+	var directInfoArr []DirectInfo
+
+	for i := range val {
+		err = json.Unmarshal([]byte(val[i]), &directInfo)
+
+		if err != nil {
+			log.Printf("failed to convert to json, %v", err)
+			return nil, err
+		}
+		directInfoArr = append(directInfoArr, directInfo)
+	}
+
+	return directInfoArr, nil
 }
 
 func (rc *RedisClient) GetKeys(key string) []string {
 	return []string{}
 }
 
-func NewRedisClient() IRedis {
+func NewRedisClient(addr string) IRedis {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     config.GoDotEnvVariable("ADDR"),
+		Addr:     addr,
 		Password: "",
 		DB:       0,
 	})
